69e34cde89e9b4865819bed1de213bfb2999edb4
CelestialAbyss <30322176+CelestialAbyss@users.noreply.github.com>
2023-05-17 05:44:24 -0400

    Version 0.5.7b repository sync
    Added some new mods, new patchouli guidebook content.

---
+++ kubejs/server_scripts/disabler.js
@@ -0,0 +1,13 @@
+onEvent('recipes', event => {
+    event.remove({id: 'minecraft:coarse_dirt'})
+    event.remove({output: 'mysticalagriculture:inferium_furnace'})
+    event.remove({output: 'mysticalagriculture:prudentium_furnace'})
+    event.remove({output: 'mysticalagriculture:tertium_furnace'})
+    event.remove({output: 'mysticalagriculture:imperium_furnace'})
+    event.remove({output: 'mysticalagriculture:supremium_furnace'})
+    event.remove({output: 'projecte:dm_furnace'})
+    event.remove({output: 'projecte:rm+furnace'})
+    event.remove({output: 'waystones:warp_plate'})
+    event.remove({output: 'waystones:attuned_shard'})
+    event.remove({output: 'waystones:warp_dust'})
+})
